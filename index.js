e8e6d4eea3ccfff71a866be553a3ff89a3bc0c84
Ori <ori100no@gmail.com>
2024-05-05 21:16:37 +0300

    a little fix in index.js that mongoose will run before the server starts

--- index.js
+++ index.js
@@ -1,30 +1,32 @@
-const express = require("express");
-const path = require("path");
-const bodyParser = require("body-parser");
-const cookieParser = require("cookie-parser");
-const cors = require("cors");
-const mongoose = require("mongoose");
-const customEnv = require("custom-env");
+const express = require('express');
+const path = require('path');
+const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
+const cors = require('cors');
+const mongoose = require('mongoose');
+const customEnv = require('custom-env');
 
-const userRouter = require("./routes/user.router");
-const graphRouter = require("./routes/graph.router");
+const userRouter = require('./routes/user.router');
+const graphRouter = require('./routes/graph.router');
 
-const newLocal = customEnv.env(process.env.NODE_ENV, "./config");
+const newLocal = customEnv.env(process.env.NODE_ENV, './config');
 
 const app = express();
 app.use(cookieParser());
 app.use(
   cors({
-    origin: "*",
+    origin: '*',
     optionsSuccessStatus: 200,
   })
 );
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static(path.join(__dirname, process.env.STATIC_PATH)));
-app.use("/graph", graphRouter);
-app.use("/auth", userRouter);
-app.listen(process.env.PORT, () =>
-  console.log(`sever is listening to port ${process.env.PORT}`)
-);
-mongoose.connect(process.env.MONGOOSE_CONNECTION_STRING);
+app.use('/graph', graphRouter);
+app.use('/auth', userRouter);
+
+mongoose.connect(process.env.MONGOOSE_CONNECTION_STRING).then(() => {
+  app.listen(process.env.PORT, () => {
+    console.log(`sever is listening to port ${process.env.PORT}`);
+  });
+});
